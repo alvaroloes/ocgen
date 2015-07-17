3201cc1292cc8310219ac6d1b04f2c0e73eabe7c
Álvaro <alvaro@theagilemonkeys.com>
2015-07-17 15:24:42 +0100

    Started the refactored to add the automatic rewriting feature

--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"errors"
-	"flag"
 	"fmt"
-	"log"
 
 	"github.com/alvaroloes/ocgen/parser"
-	"github.com/alvaroloes/ocgen/generator")
+)
 
 func main() {
+	files := parser.GetParseableFiles("./fixtures")
+	fmt.Println(files)
+
 	//TODO: In the future, this will have some flags
-	flag.Parse()
-	filenames := flag.Args()
+	// flag.Parse()
+	// filenames := flag.Args()
 
-	if len(filenames) == 0 {
-		log.Fatal(errors.New("No input files have been specified"))
-	}
+	// if len(filenames) == 0 {
+	// 	log.Fatal(errors.New("No input files have been specified"))
+	// }
 
-	for _, filename := range filenames {
-		classInfo, err := parser.ParseFile(filename)
-		if err != nil {
-			log.Printf("Error processing file %v: %v", filename, err)
-		}
+	// for _, filename := range filenames {
+	// 	classInfo, err := parser.ParseFile(filename)
+	// 	if err != nil {
+	// 		log.Printf("Error processing file %v: %v", filename, err)
+	// 	}
 
-		nsCopyingCode, err := generator.NSCopying(classInfo)
-		if (err != nil) {
-			log.Printf("Error generating NSCopying code: %v", err)
-		}
-		fmt.Println(nsCopyingCode)
+	// 	nsCopyingCode, err := generator.NSCopying(classInfo)
+	// 	if err != nil {
+	// 		log.Printf("Error generating NSCopying code: %v", err)
+	// 	}
+	// 	fmt.Println(nsCopyingCode)
 
-		nsCodingCode, err := generator.NSCoding(classInfo)
-		if (err != nil) {
-			log.Printf("Error generating NSCoding code: %v", err)
-		}
-		fmt.Println(nsCodingCode)
-	}
+	// 	nsCodingCode, err := generator.NSCoding(classInfo)
+	// 	if err != nil {
+	// 		log.Printf("Error generating NSCoding code: %v", err)
+	// 	}
+	// 	fmt.Println(nsCodingCode)
+	// }
 }
--- parser/parser.go
+++ parser/parser.go
@@ -2,11 +2,12 @@ package parser
 
 import (
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
-    "strings")
-
-
-//@property (nonatomic, copy) NSString *orderID;
+	"strings"
+)
 
 var propertyRegexp = regexp.MustCompile(`\s?@property\s?(?:\((.*)\))?\s?([^\s\*]*)\s?(\*)?(.*);`)
 
@@ -15,43 +16,68 @@ type ObjCClassInfo struct {
 }
 
 type Property struct {
-	Name,Class string
-    Attributes []string
-    IsPointer bool
+	Name, Class string
+	Attributes  []string
+	IsPointer   bool
 }
 
 // Right now we read the whole file in memory. Normally there should not be a problem
-// as source files are not extremely big. Anyway this could
-
+// as source files are not extremely big. Anyway this could be improved
 func ParseFile(filename string) (*ObjCClassInfo, error) {
 	fileBytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-    info := ObjCClassInfo{}
-	propertyMatches := propertyRegexp.FindAllSubmatch(fileBytes, -1)   
-    
-    // Extract all properties
-    for _,propertyMatch := range propertyMatches {
-        //Split the attributes string and trim each of them
-        attributes := strings.Split(string(propertyMatch[1]),",")
-        for i,attr := range attributes {
-            attributes[i] = strings.TrimSpace(attr)
-            
-        }
-        class := string(propertyMatch[2])
-        pointer := string(propertyMatch[3])
-        name := string(propertyMatch[4])
+	info := ObjCClassInfo{}
+	propertyMatches := propertyRegexp.FindAllSubmatch(fileBytes, -1)
 
-        // Add this property to the info
-        info.Properties = append(info.Properties, Property{
-            Name: name,
-            Class: class,
-            Attributes: attributes,
-            IsPointer: pointer != "",
-        })
-    }
-    
+	// Extract all properties
+	for _, propertyMatch := range propertyMatches {
+		//Split the attributes string and trim each of them
+		attributes := strings.Split(string(propertyMatch[1]), ",")
+		for i, attr := range attributes {
+			attributes[i] = strings.TrimSpace(attr)
+		}
+		class := string(propertyMatch[2])
+		pointer := string(propertyMatch[3])
+		name := string(propertyMatch[4])
+
+		// Add this property to the info
+		info.Properties = append(info.Properties, Property{
+			Name:       name,
+			Class:      class,
+			Attributes: attributes,
+			IsPointer:  pointer != "",
+		})
+	}
+
 	return &info, nil
+}
+
+func GetParseableFiles(rootPath string) []string {
+	var headerFiles []string
+
+	err := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+
+		isHeader, err := filepath.Match("*.h", info.Name())
+		if err != nil {
+			return err
+		}
+
+		if isHeader {
+			headerFiles = append(headerFiles, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return headerFiles
 }
